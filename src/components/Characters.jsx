55305d72a7724a5af13e702295d6a842c941b39f
unknown <angelicaguemes@hotmail.com>
2022-01-14 23:13:53 -0600

    [HOOK] usereducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
+
+const initialState = {
+    favoritesState: []
+}
+
+//my reducer: in charge to add characters to 'favorites'
+const favoritesReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITES':
+            console.log( ' ADD ' + action.payload.name);
+            return {
+                ...state,
+                favoritesState: [...state.favoritesState, action.payload]
+            };
+        case 'REMOVE_FROM_FAVORITES':
+            console.log( ' REMOVE ' + action.payload);
+            return {
+                ...state,
+                favoritesState: state.favoritesState.filter(item => item.id !== action.payload)
+            };
+        default:
+            return state;
+            
+    }
+}
+
 
 const Characters = () => {
     // my state
     const [characters, setCharacters] = useState([]);
+    
     // my effect
     // params: anonymous function for the logic, variable that listenes for a change
     // the array will be listening for changes in the effect and rerender when there is a change.
@@ -12,10 +39,40 @@ const Characters = () => {
             .then(data => setCharacters(data.results));
     }, []);
 
+    // my reducer
+    const [favorites, dispatch] = useReducer(favoritesReducer, initialState);
+
+    const handleClick = (action, favorite )=> {
+        dispatch({type: action, payload: favorite})
+    }
+
+
     return (
         <div className='Characters'>
+            Favorites:
+            {favorites.favoritesState.map(favorite => (
+                <ul className='item' key={'fav'+favorite.id}>
+                    <li>{favorite.name}</li>
+                    <button type="button" onClick={() => handleClick('REMOVE_FROM_FAVORITES', favorite.id)}>
+                        Remove
+                    </button>
+                </ul>
+            ))}   
+            Characters:
             {characters.map(character => (
-                <h2>{character.name}</h2>
+                <div className='item' key={character.id}>
+                    <h2>{character.name}</h2>
+                    {
+                        favorites.favoritesState.find(item => item.id === character.id) ?
+                        <button type="button" onClick={() => handleClick('REMOVE_FROM_FAVORITES',character.id)}>
+                            remove from favorites
+                        </button>
+                        :
+                        <button type="button" onClick={() => handleClick('ADD_TO_FAVORITES',character)}>
+                            Add to favorites
+                        </button>
+                    }
+                </div>
             ))}   
         </div>
     );
